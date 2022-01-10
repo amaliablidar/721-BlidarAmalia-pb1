7f7621d0af712937452415dc6a9b2092b2333957
Amalia Blidar <amalia.blidar@stud.ubbcluj.ro>
2022-01-10 16:46:26 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// /Users/amaliablidar/IdeaProjects/demo/demo1/exercitiul1-BlidarAmalia-721-1/src/com/company
+    }
+}
